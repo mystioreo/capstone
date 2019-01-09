a6637d820fdae30de3cdfa07e0133e329aecaab5
Daniela Di Iorio <danielad@gmail.com>
2019-01-08 16:50:41 -0800

    installed react native elements.  added checkboxes for equipment, but they are not linked to the exercise data filter yet.

--- pushup-app/screens/SettingsScreen.js
+++ pushup-app/screens/SettingsScreen.js
@@ -1,8 +1,25 @@
 import React from 'react';
-import { View, Button, StyleSheet, AsyncStorage } from 'react-native';
+import { Picker, View, Button, StyleSheet, AsyncStorage } from 'react-native';
 import { ExpoConfigView } from '@expo/samples';
+import { CheckBox } from 'react-native-elements';
 
 export default class SettingsScreen extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      language: "2",
+      barbell: false,
+      szbar: false,
+      dumbbell: false,
+      swissball: false,
+      pullupbar: false,
+      bench: false,
+      inclinebench: false,
+      kettlebell: false
+    }
+  }
+
   static navigationOptions = {
     title: 'Settings',
   };
@@ -13,7 +30,63 @@ export default class SettingsScreen extends React.Component {
 
     return (
         <View style={styles.container}>
-          <Button title="Actually, sign me out :)" onPress={this._signOutAsync} />
+          <Button title="Log Out" onPress={this._signOutAsync} />
+
+            <CheckBox
+              title='Barbell'
+              checked={this.state.barbell}
+              onPress={() => this.setState({barbell: !this.state.barbell})}
+            />
+
+            <CheckBox
+              title='SZ-bar'
+              checked={this.state.szbar}
+              onPress={() => this.setState({szbar: !this.state.szbar})}
+            />
+
+            <CheckBox
+              title='Dumbbell'
+              checked={this.state.dumbbell}
+              onPress={() => this.setState({dumbbell: !this.state.dumbbell})}
+            />
+
+            <CheckBox
+              title='Swiss Ball'
+              checked={this.state.swissball}
+              onPress={() => this.setState({swissball: !this.state.swissball})}
+            />
+
+            <CheckBox
+              title='Pull-up Bar'
+              checked={this.state.pullupbar}
+              onPress={() => this.setState({pullupbar: !this.state.pullupbar})}
+            />
+
+            <CheckBox
+              title='Bench'
+              checked={this.state.bench}
+              onPress={() => this.setState({bench: !this.state.bench})}
+            />
+
+            <CheckBox
+              title='Incline Bench'
+              checked={this.state.inclinebench}
+              onPress={() => this.setState({inclinebench: !this.state.inclinebench})}
+            />
+
+            <CheckBox
+              title='Kettlebell'
+              checked={this.state.kettlebell}
+              onPress={() => this.setState({kettlebell: !this.state.kettlebell})}
+            />
+
+            <Picker
+              selectedValue={this.state.language}
+              style={{ height: 50, width: 100 }}
+              onValueChange={(itemValue, itemIndex) => this.setState({language: itemValue})}>
+              <Picker.Item label="Deutsch" value="1" />
+              <Picker.Item label="English" value="2" />
+            </Picker>
         </View>
       );
   }
