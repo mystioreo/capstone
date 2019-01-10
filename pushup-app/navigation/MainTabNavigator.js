4de53ad4293c9413c86f6f9eaff6d87c4c5a69c3
Daniela Di Iorio <danielad@gmail.com>
2019-01-10 15:51:13 -0800

    changed tab bar icons

--- pushup-app/navigation/MainTabNavigator.js
+++ pushup-app/navigation/MainTabNavigator.js
@@ -17,8 +17,8 @@ HomeStack.navigationOptions = {
       focused={focused}
       name={
         Platform.OS === 'ios'
-          ? `ios-information-circle${focused ? '' : '-outline'}`
-          : 'md-information-circle'
+          ? `ios-wine`
+          : 'md-wine'
       }
     />
   ),
@@ -33,7 +33,7 @@ SettingsStack.navigationOptions = {
   tabBarIcon: ({ focused }) => (
     <TabBarIcon
       focused={focused}
-      name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
+      name={Platform.OS === 'ios' ? 'ios-construct' : 'md-construct'}
     />
   ),
 };
